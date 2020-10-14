3ed1b406117a5049e5172f7ad8ce3eab239abf8b
viettran <viet.q.tran@gmail.com>
2020-10-14 06:52:31 -0700

    Create project

---
+++ CollectionViewTableCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTableCell
+//
+//  Created by Viet Tran on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
